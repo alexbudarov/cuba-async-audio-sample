3e952a68b11ffb4269ae9e87a2d6e31b1698c5f1
budarov <budarov@haulmont.com>
2021-09-24 13:54:13 +0400

    working audio sample with @Async

---
+++ modules/web/src/com/company/audio/web/controller/AudioController.java
@@ -0,0 +1,50 @@
+package com.company.audio.web.controller;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/audio/v1")
+public class AudioController {
+
+    @GetMapping("/audio")
+    @Async
+    public ResponseEntity<StreamingResponseBody> downloadAudio() throws IOException {
+        String fileName = "preamble10.wav";
+        int fileSize;
+        try (InputStream inputStream = getInputStream(fileName)) {
+            ByteArrayOutputStream fullFile = new ByteArrayOutputStream();
+            fileSize = IOUtils.copy(inputStream, fullFile);
+        }
+
+        InputStream inputStream = getInputStream(fileName);
+        StreamingResponseBody responseBody = outputStream -> FileCopyUtils.copy(inputStream, outputStream);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+//                    .header("Content-Type", "audio/wav")
+                .contentLength(fileSize)
+                .header("accept-ranges", "bytes")
+                .body(responseBody);
+    }
+
+    private InputStream getInputStream(String fileName) {
+        InputStream inputStream = getClass().getResourceAsStream("/com/company/audio/web/audio/" + fileName);
+        Objects.requireNonNull(inputStream);
+        return inputStream;
+    }
+}
